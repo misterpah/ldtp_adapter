8d6f403dba5473123ece5b93b8c9967972dc5e4f
ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>
2018-03-13 10:30:45 +0800

    naming convention between X and PyUserInput were similar but different. utilize similarity to compare them
    Signed-off-by: ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>

--- keyboard.py
+++ keyboard.py
@@ -1,10 +1,53 @@
+import re
+def _find_key(keyString):
+    k = PyKeyboard()
+    key_to_press = None
+    highest = 0
+    if keyString.startswith("<"):
+        cur_key = keyString[1:-1]
+        for each in dir(k):
+            if each.endswith("_key"):
+                if similar(cur_key + "_key" ,each) > highest:
+                    highest = similar(cur_key + "_key" ,each)
+                    key_to_press = getattr(k,each)
+    else:
+        key_to_press = keyString
+        #key_to_press = k.lookup_character_keycode(keyString)
+        #if keyString == "_":
+        #    key_to_press = [k.shift_key,k.lookup_character_keycode(keyString)]
+            
+        
+    return key_to_press
+    
 def keypress(key):
-	print "{} :not implemented yet".format(sys._getframe().f_code.co_name)
-	
+    k = PyKeyboard()
+    key_to_press = _find_key(key)
+    if key_to_press != None:
+        k.press_key(key_to_press)
 
 def keyrelease(key):
-	print "{} :not implemented yet".format(sys._getframe().f_code.co_name)
 
+    k = PyKeyboard()
+    key_to_press = _find_key(key)
+    if key_to_press != None:
+        k.release_key(key_to_press)
+
+def _press_key(key):
+    keypress(key)
+    keyrelease(key)
+    time.sleep(0.3)
 
 def generatekeyevent(key):
-	print "{} :not implemented yet".format(sys._getframe().f_code.co_name)
+    #print "{} :not implemented yet".format(sys._getframe().f_code.co_name)
+    key_to_press =[]
+    if key.startswith("<"):
+        regex = r"(<.[^(><.)]+>)"
+        key = re.findall(regex,key)
+    for each in key:
+        key_to_press.append(_find_key(each))
+    try:
+        for each in key_to_press:
+            _press_key(each)
+    except TypeError:
+        _press_key(key_to_press)
+
