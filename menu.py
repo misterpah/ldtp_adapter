69f2cd49768772d3e47c864a38f483075869e879
ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>
2018-03-20 15:05:07 +0800

    add selectmenuitem function
    Signed-off-by: ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>

--- menu.py
+++ menu.py
@@ -1,2 +1,18 @@
 def selectmenuitem(window,object):
-	print "{} :not implemented yet".format(sys._getframe().f_code.co_name)
+    log("{} :not implemented yet".format(sys._getframe().f_code.co_name))
+    object = object.split(";")
+    debug(window,object)
+    debug(pyacc.getObject(window,object[0]))
+    if len(object) == 2:
+        mousemove(window,object[0])
+        ldtp_extend_mouse_click_here()
+        time.sleep(1)
+        objectHandle = pyacc.getObject(window,object[1])
+        try:
+            objectHandle[0]
+            #debug(objectHandle[1]['return'])
+            mousemove(window,object[1],handle=objectHandle[1]['return']['handle'])
+            ldtp_extend_mouse_click_here()
+        except IndexError:
+            return False
+
