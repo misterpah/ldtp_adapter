65d0ebdc0df00dc06e6f7549e9b6614be0a5c912
ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>
2018-03-20 15:05:36 +0800

    add mouse delay and logging function
    Signed-off-by: ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>

--- mouse.py
+++ mouse.py
@@ -1,15 +1,27 @@
-def mousemove(window,object):
-	pyacc.mouseMove(window,object)
-		
+def mousemove(window,object,handle=None):
+	debug(window,object)
+	pyacc.mouseMove(window,object,handle)
+	time.sleep(MOUSEDELAY)
+
+def mouseleftclick(window,object):
+    debug(window,object)
+    click(window,object)
+    log("{} :not implemented yet".format(sys._getframe().f_code.co_name))
+    time.sleep(MOUSEDELAY)
+
 def click(window,object):
+	debug(window,object)
 	pyacc.mouseClick(window,object)
+	time.sleep(MOUSEDELAY)
 			
 def ldtp_extend_mouse_click_here():
 	m = PyMouse()
 	pos = m.position()
 	m.click(pos[0],pos[1],1)
+	time.sleep(MOUSEDELAY)
 	
 def ldtp_extend_mouse_right_click_here():
 	m = PyMouse()
 	pos = m.position()
 	m.click(pos[0],pos[1],2)
+	time.sleep(MOUSEDELAY)
