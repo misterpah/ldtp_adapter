540cabc0111783465ea3d12d9feccf2f7ac62885
ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>
2018-03-20 15:04:16 +0800

    properly add error object
    Signed-off-by: ABDUL MUTTALIB, ABDUL ARIF <abdul.arif.b.abdul.muttalib@intel.com>

--- error.py
+++ error.py
@@ -1,4 +1,11 @@
-class client_exception(Exception):
-    LdtpExecutionError = ""
+class LdtpExecutionError(Exception):
     def __init__(self,*args,**kwargs):
         Exception.__init__(self,*args,**kwargs)
+        
+class client_exception(Exception):
+    LdtpExecutionError = LdtpExecutionError
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+        
+        
+
